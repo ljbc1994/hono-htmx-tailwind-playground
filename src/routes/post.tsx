6cb3fcf8e8f7f3fedb457fc57b53df4ef8b44645
Louie Colgan <ljbc94@gmail.com>
2023-11-10 11:22:20 +0000

    test blog from gh

--- src/routes/post.tsx
+++ src/routes/post.tsx
@@ -1,14 +1,38 @@
-import { Context, Env } from "hono";
-import { marked } from 'marked';
+import { Context } from "hono";
+import { marked } from "marked";
 
 import Post from "../pages/Post";
+import { Env } from "../types/Bindings";
 
 export const post = async (c: Context<Env, "/post/:id", {}>) => {
-  const id = c.req.param('id')
+  const id = c.req.param("id");
 
-  const html = marked.parse(`# Blog Post ${id}\n\nRendered by **marked**.`)
+  const path = `posts/${id}.md`;
 
-  c.res.headers.set('Cache-Control', 'public, s-maxage=604800');
+  try {
+    const response = await fetch(
+      `https://api.github.com/repos/${c.env?.GH_OWNER}/${c.env?.GH_REPO}/contents/${path}`,
+      {
+        headers: {
+          Authorization: `Bearer ${c.env?.GH_AUTH_TOKEN}`,
+        },
+      }
+    );
 
-  return c.render(<Post content={html} />, { title: 'Example' });
+    if (!response.ok) {
+      return c.render(<Post content="Not found" />, { title: "Not Found" });
+    }
+
+    const data = (await response.json()) as { content: string };
+
+    const content = Buffer.from(data.content, "base64").toString("utf-8");
+
+    const html = marked.parse(content);
+
+    c.res.headers.set("Cache-Control", "public, s-maxage=604800");
+
+    return c.render(<Post content={html} />, { title: "Example" });
+  } catch (err) {
+    return c.render(<Post content="Error" />, { title: "Not Found" });
+  }
 };
---
+++ src/types/Bindings.ts
@@ -0,0 +1,10 @@
+export type Env = {
+    Bindings?: Bindings;
+    Variables?: Record<string, unknown>;
+}
+
+export type Bindings = {
+  GH_OWNER: string;
+  GH_REPO: string;
+  GH_AUTH_TOKEN: string;
+};
